a4703dc9bb09d68397061f43019e3d16b12b682f
Alex <shanko.becz@gmail.com>
2017-04-25 14:15:53 -0600

    Add files via upload

---
+++ testFile_1.py
@@ -0,0 +1,20 @@
+#test for the new PythonProg
+
+print "__Welcome to my Number Game__"
+
+x = int(input("__Give a number in rng 1-5: "))
+
+if x == 1:
+	print "Couldn't you find better?" 
+	
+elif x == 2:
+	print "Couldn't you find bigger?"
+
+elif x == 3:
+	print "Wouldn't you find digger?"
+	
+elif x == 4:
+	print "Hadn't you had enough?"
+	
+else:
+	print "Hooray!"
